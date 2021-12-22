5e404600dd1cfc541f21c06b37bb849a146b73b8
Mohamed Ramadan <m.aboalhoda@hotmail.com>
2021-12-22 14:38:41 -0500

    some adjustment and adding more data

--- ADACO_Checklog/ADACO_Checklog.py
+++ ADACO_Checklog/ADACO_Checklog.py
@@ -1,8 +1,10 @@
 import csv
-import os
+import os, pprint
+from depts import departments, expense_code
 
 
 def row_cleaner(row):
+    """This function is to clear all the empty vlaued elements in each row"""
     clear = True
     while clear:
         for cell in row:
@@ -14,18 +16,19 @@ def row_cleaner(row):
 
 
 def index_finder(list, elem):
+    """this function is to get the index of the targeted element in the nested list"""
     for sub_list in list:
         if elem in sub_list:
             return list.index(sub_list)
             
 
 
-path = r"S:\Accounting\Javier\Checkbook\outlets sheets\2021\December"
+path = r"file location"
 
 os.chdir(path)
 
 asset_files = os.listdir()
-print(asset_files)
+#print(asset_files)
 new_lines = []
 for file in asset_files:
 
@@ -34,21 +37,52 @@ for file in asset_files:
         with open(file) as file:
             file_reader = csv.reader(file, dialect="excel")
             line_list = []
+            #here we clean the row from the empty values and make nested list for each row to loop through it
             for row in file_reader:
                 clean_row = row_cleaner(row)
                 if len(clean_row) > 0:
                     line_list.append(clean_row)
-                #print(row)
+                #print(clean_row)
                 #print(len(row))
+            #below we get the indexs for the lines that we need to loop in and fitch the vlaues targeted
+
             start_line = index_finder(line_list, 'Other Expenses')
             end_line = index_finder(line_list, 'Total Other Expenses')
-   
-            print(start_line, end_line)
-            for elem in line_list[start_line:end_line]:
+            
 
-                new_lines.append(elem[5:])
+            #in the below we get the department code to map it in the system
+            department_code = ''
+            for elem in line_list:
+                for key in departments:
+                    if key == elem[0]:
+                        department_code = departments[elem[0]]
+                        #print(departments[elem[0]])
+
+            #here we get the account code
+            #for elem in line_list:
+            #    if len(line_list) > 1:
+            #        for key in expense_code:
+            #            if key == elem[0]:
+            #                print(key, expense_code[elem[0]])
+
+            
+            #print(start_line, end_line)
+        for elem in line_list[start_line:end_line]:
+            line = []
+            if len(elem) > 1:
+                line.insert(0,department_code)
+                for key in expense_code:
+                    if key == elem[0]:
+                        line.insert(1,expense_code[elem[0]])
+                line.extend(elem[5:])
+
+                new_lines.append(line)
         #print(new_lines)
+
 
 with open(path+'\\'+"cleared_data.csv", "w", newline="") as new_file:
     writer = csv.writer(new_file)
     writer.writerows(new_lines)
+
+if __name__ == "__main__":
+    __name__
